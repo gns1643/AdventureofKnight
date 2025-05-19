8b65a18c26242fb09eb40f71cf23cb39c46f20a4
gns1643 <gns1643@naver.com>
2025-05-19 17:33:24 +0900

    캐릭터 애니메이션 수정, 카메라 추가

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -40,7 +40,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void Flip()
     {
-        facingDirection += -1;
+        facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1,
             transform.localScale.y, transform.localScale.z);
     }
